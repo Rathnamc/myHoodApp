db5be8208c4b20da88676db2d4a9a362648e40b3
Christopher Rathnam <Rathnam.C@gmail.com>
2015-12-23 01:24:26 -0500

    Initial Commit

---
+++ my-hood-CRDevs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-hood-CRDevs
+//
+//  Created by Christopher Rathnam on 12/23/15.
+//  Copyright © 2015 Christopher Rathnam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
